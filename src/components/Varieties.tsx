142289277b4c148291a084363f9c2a053bed7867
maheeen-khan <maheeenkhan4@gmail.com>
2024-12-20 07:49:00 -0800

    updated

--- src/components/Varieties.tsx
+++ src/components/Varieties.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import smallIcon from '../assets/small-icon.png'
 import third1 from '../assets/third1.png'
-
+import third2 from '../assets/third2.png'
+import third3 from '../assets/Third3.png'
 const Varieties = () => {
     return (
         <>
@@ -10,140 +11,140 @@ const Varieties = () => {
 
                 <div className="row">
 
-                <p style={{color:'red'}} className='mt-3'>SPECIAL DISHES</p>
-                <h2 className='main-heading mt-1 mb-5'>Standout Dishes From Our Menu</h2>
+                    <p style={{ color: 'red' }} className='mt-3'>SPECIAL DISHES</p>
+                    <h2 className='main-heading mt-1 mb-5'>Standout Dishes From Our Menu</h2>
 
-<div className="scrollmenu d-flex carousel slide">
-                    <div className="col-lg-3 col-md-3 col-sm-12">
+                    <div className="scrollmenu d-flex carousel slide">
+                        <div className="col-lg-3 col-md-3 col-sm-12">
 
-                        <div class="myCard text-center">
-                            <a class="card1" href="#">
-                                {/* <p>This is heading</p>
+                            <div className="myCard text-center">
+                                <a className="card1" href="#">
+                                    {/* <p>This is heading</p>
                                 <p class="small">Card description with lots of great facts and interesting details.</p> */}
-                                 <img src={third1} class="card-img-top" width={50}/>
-                              
-                                <div class="card-body">
-                                    <br />
-                                    <h5 class="card-title">Fattoush salad</h5>
-                                    {/* <p class="card-text">Description of the item</p> */}
-                                    <p><span style={{color:'red'}}>$</span>24.00</p>
-                                </div>
-                                <div class="go-corner" href="#">
-                                    <div class="go-arrow">
-                                        <img src={smallIcon} alt="" width={10} />
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12">
-                    <div class="myCard text-center">
-                            <a class="card1" href="#">
-                                {/* <p>This is heading</p>
-                                <p class="small">Card description with lots of great facts and interesting details.</p> */}
-                                 <img src={third1} class="card-img-top" width={50}/>
-                              
-                                <div class="card-body">
-                                    <br />
-                                    <h5 class="card-title">Fattoush salad</h5>
-                                    {/* <p class="card-text">Description of the item</p> */}
-                                    <p><span style={{color:'red'}}>$</span>24.00</p>
-                                </div>
-                                <div class="go-corner" href="#">
-                                    <div class="go-arrow">
-                                        <img src={smallIcon} alt="" width={10} />
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12">
-                    <div class="myCard text-center">
-                            <a class="card1" href="#">
-                                {/* <p>This is heading</p>
-                                <p class="small">Card description with lots of great facts and interesting details.</p> */}
-                                 <img src={third1} class="card-img-top" width={50}/>
-                              
-                                <div class="card-body">
-                                    <br />
-                                    <h5 class="card-title">Fattoush salad</h5>
-                                    {/* <p class="card-text">Description of the item</p> */}
-                                    <p><span style={{color:'red'}}>$</span>24.00</p>
-                                </div>
-                                <div class="go-corner" href="#">
-                                    <div class="go-arrow">
-                                        <img src={smallIcon} alt="" width={10} />
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-md-3 col-sm-12">
-                    <div class="myCard text-center">
-                            <a class="card1" href="#">
-                                {/* <p>This is heading</p>
-                                <p class="small">Card description with lots of great facts and interesting details.</p> */}
-                                 <img src={third1} class="card-img-top" width={50}/>
-                              
-                                <div class="card-body">
-                                    <br />
-                                    <h5 class="card-title">Fattoush salad</h5>
-                                    {/* <p class="card-text">Description of the item</p> */}
-                                    <p><span style={{color:'red'}}>$</span>24.00</p>
-                                </div>
-                                <div class="go-corner" href="#">
-                                    <div class="go-arrow">
-                                        <img src={smallIcon} alt="" width={10} />
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
+                                    <img src={third1} className="card-img-top" width={50} />
 
-                    <div className="col-lg-3 col-md-3 col-sm-12">
-                    <div class="myCard text-center">
-                            <a class="card1" href="#">
-                                {/* <p>This is heading</p>
-                                <p class="small">Card description with lots of great facts and interesting details.</p> */}
-                                 <img src={third1} class="card-img-top" width={50}/>
-                              
-                                <div class="card-body">
-                                    <br />
-                                    <h5 class="card-title">Fattoush salad</h5>
-                                    {/* <p class="card-text">Description of the item</p> */}
-                                    <p><span style={{color:'red'}}>$</span>24.00</p>
-                                </div>
-                                <div class="go-corner" href="#">
-                                    <div class="go-arrow">
-                                        <img src={smallIcon} alt="" width={10} />
+                                    <div className="card-body">
+                                        <br />
+                                        <h5 className="card-title">Fattoush salad</h5>
+                                        {/* <p class="card-text">Description of the item</p> */}
+                                        <p><span style={{ color: 'red' }}>$</span>24.00</p>
                                     </div>
-                                </div>
-                            </a>
+                                    <div className="go-corner">
+                                        <div className="go-arrow">
+                                            <img src={smallIcon} alt="" width={10} />
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
                         </div>
-                    </div>
+                        <div className="col-lg-3 col-md-3 col-sm-12">
+                            <div className="myCard text-center">
+                                <a className="card1" href="#">
+                                    {/* <p>This is heading</p>
+                                <p class="small">Card description with lots of great facts and interesting details.</p> */}
+                                    <img src={third2} className="card-img-top" width={50} />
+
+                                    <div className="card-body">
+                                        <br />
+                                        <h5 className="card-title">Fattoush salad</h5>
+                                        {/* <p class="card-text">Description of the item</p> */}
+                                        <p><span style={{ color: 'red' }}>$</span>24.00</p>
+                                    </div>
+                                    <div className="go-corner">
+                                        <div className="go-arrow">
+                                            <img src={smallIcon} alt="" width={10} />
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-12">
+                            <div className="myCard text-center">
+                                <a className="card1" href="#">
+                                    {/* <p>This is heading</p>
+                                <p class="small">Card description with lots of great facts and interesting details.</p> */}
+                                    <img src={third3} className="card-img-top" width={50} />
+
+                                    <div className="card-body">
+                                        <br />
+                                        <h5 className="card-title">Fattoush salad</h5>
+                                        {/* <p class="card-text">Description of the item</p> */}
+                                        <p><span style={{ color: 'red' }}>$</span>24.00</p>
+                                    </div>
+                                    <div className="go-corner">
+                                        <div className="go-arrow">
+                                            <img src={smallIcon} alt="" width={10} />
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-lg-3 col-md-3 col-sm-12">
+                            <div className="myCard text-center">
+                                <a className="card1" href="#">
+                                    {/* <p>This is heading</p>
+                                <p class="small">Card description with lots of great facts and interesting details.</p> */}
+                                    <img src={third1} className="card-img-top" width={50} />
+
+                                    <div className="card-body">
+                                        <br />
+                                        <h5 className="card-title">Fattoush salad</h5>
+                                        {/* <p class="card-text">Description of the item</p> */}
+                                        <p><span style={{ color: 'red' }}>$</span>24.00</p>
+                                    </div>
+                                    <div className="go-corner">
+                                        <div className="go-arrow">
+                                            <img src={smallIcon} alt="" width={10} />
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-3 col-md-3 col-sm-12">
+                            <div className="myCard text-center">
+                                <a className="card1" href="#">
+                                    {/* <p>This is heading</p>
+                                <p class="small">Card description with lots of great facts and interesting details.</p> */}
+                                    <img src={third1} className="card-img-top" width={50} />
+
+                                    <div className="card-body">
+                                        <br />
+                                        <h5 className="card-title">Fattoush salad</h5>
+                                        {/* <p class="card-text">Description of the item</p> */}
+                                        <p><span style={{ color: 'red' }}>$</span>24.00</p>
+                                    </div>
+                                    <div className="go-corner">
+                                        <div className="go-arrow">
+                                            <img src={smallIcon} alt="" width={10} />
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
 
 
-                    <div className="col-lg-3 col-md-3 col-sm-12">
-                    <div class="myCard text-center">
-                            <a class="card1" href="#">
-                                {/* <p>This is heading</p>
+                        <div className="col-lg-3 col-md-3 col-sm-12">
+                            <div className="myCard text-center">
+                                <a className="card1" href="#">
+                                    {/* <p>This is heading</p>
                                 <p class="small">Card description with lots of great facts and interesting details.</p> */}
-                                 <img src={third1} class="card-img-top" width={50}/>
-                              
-                                <div class="card-body">
-                                    <br />
-                                    <h5 class="card-title">Fattoush salad</h5>
-                                    {/* <p class="card-text">Description of the item</p> */}
-                                    <p><span style={{color:'red'}}>$</span>24.00</p>
-                                </div>
-                                <div class="go-corner" href="#">
-                                    <div class="go-arrow">
-                                        <img src={smallIcon} alt="" width={10} />
+                                    <img src={third1} className="card-img-top" width={50} />
+
+                                    <div className="card-body">
+                                        <br />
+                                        <h5 className="card-title">Fattoush salad</h5>
+                                        {/* <p class="card-text">Description of the item</p> */}
+                                        <p><span style={{ color: 'red' }}>$</span>24.00</p>
                                     </div>
-                                </div>
-                            </a>
+                                    <div className="go-corner">
+                                        <div className="go-arrow">
+                                            <img src={smallIcon} alt="" width={10} />
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
                         </div>
-                    </div>
 
                     </div>
                 </div>
