747df9b79cadd17c3db93b1aae8c619ef8a50d2a
maheeen-khan <maheeenkhan4@gmail.com>
2024-12-26 00:39:38 -0800

    updated

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -9,15 +9,52 @@ import FifthSectionCard from './innerComponents/FifthSecCard.jsx'
 const OurServices = () => {
     return (
         <>
-            <div className="container pt-5">
+            <div className="container py-5">
 
                 <div className="row mt-3 mb-3">
                     <div className="col col-lg-5 col-md-6 col-sm-12 align-content-center">
                         <p style={{ color: 'red', fontSize: '13px', fontFamily: 'monospace' }}>OUR STORY AND SERVICES</p>
                         <h2 className='main-heading my-3'>Our Culinary Journey And Services</h2>
-                        <p style={{color:'#555555'}}>Rooted in passion, we curate unforgettable dining experiences and offer exceptional services, blending culinary artistry with warm hospitality.</p>
+                        <p style={{ color: '#555555' }}>Rooted in passion, we curate unforgettable dining experiences and offer exceptional services, blending culinary artistry with warm hospitality.</p>
                         {/* <button mat={'Explore'}/> */}
-                        <Button mat={'Explore'} className='expBtn' />
+                        {/* <Button mat={'Explore'} className='expBtn' /> */}
+
+                        {/* button */}
+
+                        
+                        <button href="#" class="button exp-button mt-4" style={{color:' #7808d0'}}>
+                            <span class="button__icon-wrapper">
+                                <svg
+                                    viewBox="0 0 14 15"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="button__icon-svg"
+                                    width="10"
+                                >
+                                    <path
+                                        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                        fill="currentColor"
+                                    ></path>
+                                </svg>
+
+                                <svg
+                                    viewBox="0 0 14 15"
+                                    fill="none"
+                                    width="10"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="button__icon-svg button__icon-svg--copy"
+                                >
+                                    <path
+                                        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                        fill="currentColor"
+                                    ></path>
+                                </svg>
+                            </span>
+                            Explore All
+                        </button>
+
+
+                        {/* button end */}
                     </div>
                     <div className="col col-lg-7 col-md-6 col-sm-12">
                         <div className="both-div d-flex flex-wrap">
