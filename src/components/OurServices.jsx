fa6572d3fba4158edf4428d7eab0df200655f1c3
maheeen-khan <maheeenkhan4@gmail.com>
2024-12-26 05:11:31 -0800

    animation added

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -16,10 +16,7 @@ const OurServices = () => {
                         <p style={{ color: 'red', fontSize: '13px', fontFamily: 'monospace' }}>OUR STORY AND SERVICES</p>
                         <h2 className='main-heading my-3'>Our Culinary Journey And Services</h2>
                         <p style={{ color: '#555555' }}>Rooted in passion, we curate unforgettable dining experiences and offer exceptional services, blending culinary artistry with warm hospitality.</p>
-                        {/* <button mat={'Explore'}/> */}
-                        {/* <Button mat={'Explore'} className='expBtn' /> */}
-
-                        {/* button */}
+                        
 
                         
                         <button href="#" class="button exp-button mt-4" style={{color:' #7808d0'}}>
@@ -56,55 +53,20 @@ const OurServices = () => {
 
                         {/* button end */}
                     </div>
-                    <div className="col col-lg-7 col-md-6 col-sm-12">
+                    <div className="col col-lg-7 col-md-6 col-sm-12" data-aos="fade-down" data-aos-easing="ease-in">
                         <div className="both-div d-flex flex-wrap">
-
-                            {/* <div class="card text-center p-4 m-2" style={{width: '18rem'}}>
-                                <img class="card-img mx-auto" src={catering} alt="Card image cap"/>
-                                    <div class="card-body">
-                                        <h5 class="card-title fifth-card-title">Catering</h5>
-                                        <p class="card-text card-things">Delight your guests with our flavors and  presentation</p>
-                                    
-                                    </div>
-                            </div> */}
 
                             <FifthSectionCard name={'Catering'} para={'Delight your guests with our flavors and  presentation'} img={catering} />
 
-
-                            {/* <div class="card text-center p-4 m-2" style={{width: '18rem'}}>
-                                <img class="card-img mx-auto" src={delivery} alt="Card image cap"/>
-                                    <div class="card-body">
-                                        <h5 class="card-title fifth-card-title">Fast delivery</h5>
-                                        <p class="card-text card-things">We deliver your order promptly to your door</p>
-                                    
-                                    </div>
-                            </div> */}
                             <FifthSectionCard name={'Fast delivery'} para={'We deliver your order promptly to your door'} img={delivery} />
 
                         </div>
 
 
-                        <div className="both-div d-flex flex-wrap">
+                        <div className="both-div d-flex flex-wrap" data-aos="fade-up" data-aos-easing="ease-in">
 
-                            {/* <div class="card text-center p-4 m-2" style={{ width: '18rem' }}>
-                                <img class="card-img mx-auto" src={gift} alt="Card image cap" />
-                                <div class="card-body">
-                                    <h5 class="card-title fifth-card-title">Online Ordering</h5>
-                                    <p class="card-text card-things">Delight your guests with our flavors and  presentation</p>
-
-                                </div>
-                            </div> */}
                             <FifthSectionCard name={'Online Ordering'} para={'Delight your guests with our flavors and  presentation'} img={gift} />
 
-
-                            {/* <div class="card text-center p-4 m-2" style={{ width: '18rem' }}>
-                                <img class="card-img mx-auto" src={ordering} alt="Card image cap" />
-                                <div class="card-body">
-                                    <h5 class="card-title fifth-card-title">Gift Cards</h5>
-                                    <p class="card-text card-things">Give the gift of exceptional dining with Foodi Gift Cards</p>
-
-                                </div>
-                            </div> */}
                             <FifthSectionCard name={'Gift Cards'} para={'Give the gift of exceptional dining with Foodi Gift Cards'} img={ordering} />
 
                         </div>
--- src/components/PopularCategories.jsx
+++ src/components/PopularCategories.jsx
@@ -14,17 +14,17 @@ const PopularCategories = () => {
                     <p className='sec2-para mt-5' style={{ color: 'red', fontSize: '13px', fontFamily: 'monospace' }}>Customer Favourite</p>
                     <h2 className='main-heading mb-5'>Popular Categories</h2>
 
-                    <div className="col-lg-3 col-md-6 col-sm-12 my-3">
+                    <div className="col-lg-3 col-md-6 col-sm-12 my-3" data-aos="fade-down" data-aos-easing="ease-in">
 
-                        <SecondSecCard name={'Main Dish'} var={'86 dishes'} img={foodItem1} imgWidth={120}/>
+                        <SecondSecCard name={'Main Dish'} var={'86 dishes'} img={foodItem1} imgWidth={120} />
 
                     </div>
-                    <div className="col-lg-3 col-md-6 col-sm-12 my-3">
+                    <div className="col-lg-3 col-md-6 col-sm-12 my-3" data-aos="fade-up" data-aos-easing="ease-in">
 
                         <SecondSecCard name={'Break Fast'} var={'12 break fast'} img={foodItem2} imgWidth={85}/>
 
                     </div>
-                    <div className="col-lg-3 col-md-6 col-sm-12 my-3">
+                    <div className="col-lg-3 col-md-6 col-sm-12 my-3" data-aos="fade-down" data-aos-easing="ease-in">
 
                         <SecondSecCard name={'Dessert'} var={'48 dessert'} img={foodItem3} imgWidth={85}/>
 
