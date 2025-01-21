c2bc064e0841b467de481cdace2bb51be6e686fa
maheeen-khan <maheeenkhan4@gmail.com>
2025-01-22 00:44:20 +0500

    updated

--- src/components/innerComponents/FifthSecCard.jsx
+++ src/components/innerComponents/FifthSecCard.jsx
@@ -4,7 +4,7 @@ const FifthSectionCard = (mycard) => {
     return (
         <>
 
-            <div class="card text-center p-4 m-2" style={{ width: '18rem' }}>
+            <div class="card text-center p-4 m-2 mx-auto" style={{ width: '18rem' }}>
                 <img class="card-img mx-auto" src={mycard.img} alt="Card image cap" />
                 <div class="card-body">
                     <h5 class="card-title fifth-card-title">{mycard.name}</h5>
--- src/components/innerComponents/SecondSecCard.jsx
+++ src/components/innerComponents/SecondSecCard.jsx
@@ -4,7 +4,7 @@ import Button from './Button.jsx'
 const SecondSecCard = (props) => {
     return (
         <>
-            <div class="card p-3" style={{ width: '18rem' }}>
+            <div class="card p-3 mx-auto" style={{ width: '18rem' }}>
                 <div className="img mx-auto">
                     <img class="card-img-top" src={props.img} alt="Burger" width={props.imgWidth} className='mx-auto img-fluid' style={{ marginTop: '15px' }} />
                 </div>
